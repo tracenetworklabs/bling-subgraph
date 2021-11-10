19e3efb0a1d74f5fe0f3d1eed62f78976d5329df
Keerthana Ramalingam <keerthu.ram28@gmail.com>
2021-11-10 17:31:19 +0530

    Updated

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,19 +10,7 @@ import {
   Minted as MintedEvent,
 } from "../generated/templates/Collection/Collection";
 
-<<<<<<< HEAD
 import { Collection as collectionContract } from "../generated/templates";
-=======
-import {
-  Market as MarketContract,
-  ReserveAuctionCreated as AuctionEvent,
-  ReserveAuctionBidPlaced as BidsEvent,
-  ReserveAuctionUpdated as ReserveAuctionUpdatedEvent,
-  ReserveAuctionCanceled as ReserveAuctionCanceledEvent,
-  ReserveAuctionFinalized as ReserveAuctionFinalizedEvent,
-} from "../generated/Market/Market";
-import { Minted, Auction, Bid } from "../generated/schema";
->>>>>>> 24efe02af8ac40ae70aa04bf733d35e6567ee47e
 
 import { Collection, Master } from "../generated/schema";
 
@@ -101,61 +89,7 @@ export function handleMinted(event: MintedEvent): void {
       event.transaction.hash.toHexString();
     token.NFTtimestamp = event.block.timestamp;
 
-<<<<<<< HEAD
     token.NFTaction = "Token Minted";
-=======
-    if (temp == null) temp = ipfs.cat(token.ipfsHash);
-
-    if (temp == null) temp = ipfs.cat(token.ipfsHash);
-
-    token.nftInfo = temp.toString();
-  }
-  token.save();
-}
-
-export function handleUpdated(event: UpdatedEvent): void {
-  let token = Minted.load(event.params.tokenId.toString());
-  let tokenContract = NFTContract.bind(event.address);
-  token.ipfsHash = tokenContract
-    .getTokenIPFSPath(event.params.tokenId)
-    .toString();
-  token.tokenURI = tokenContract.tokenURI(event.params.tokenId).toString();
-  let temp = ipfs.cat(token.ipfsHash);
-  token.nftInfo = temp.toString();
-  token.save();
-}
-
-export function handleReserveAuctionCreated(event: AuctionEvent): void {
-  let auction = Auction.load(event.params.auctionId.toString());
-  if (!auction) {
-    auction = new Auction(event.params.auctionId.toString());
-    auction.seller = event.params.seller.toHexString();
-    auction.tokenId = event.params.tokenId.toString();
-    auction.NFTContractAddress = event.params.nftContract.toHexString();
-    auction.auctionID = event.params.auctionId.toString();
-    auction.reservePrice = event.params.reservePrice;
-    auction.transactionHash =
-      "https://mumbai.polygonscan.com/tx/" +
-      event.transaction.hash.toHexString();
-    auction.timestamp = event.block.timestamp;
-    auction.bidList = [];
-    auction.action = "Auction Started";
-
-    let Mcontract = MarketContract.bind(event.address);
-    let result = Mcontract.getReserveAuction(event.params.auctionId);
-    auction.startTime = result.startTime;
-    auction.endTime = result.endTime;
-    auction.bidder = result.bidder;
-
-    let token = Minted.load(event.params.tokenId.toString());
-    token.auctionID = event.params.auctionId.toString();
-    token.auctionDetails = event.params.auctionId.toString();
-    token.owner = event.address;
-    token.save();
-  }
-  auction.save();
-}
->>>>>>> 24efe02af8ac40ae70aa04bf733d35e6567ee47e
 
     if (temp == null) temp = ipfs.cat(token.ipfsHash);
 
@@ -164,32 +98,5 @@ export function handleReserveAuctionCreated(event: AuctionEvent): void {
     token.nftInfo = temp.toString();
   }
 
-<<<<<<< HEAD
   token.save();
-=======
-export function handleReserveAuctionCanceled(event: ReserveAuctionCanceledEvent): void {
-  let auction = Auction.load(event.params.auctionId.toString());
-  let Mcontract = MarketContract.bind(event.address);
-  let result = Mcontract.getReserveAuction(event.params.auctionId);
-  let token = Minted.load(result.tokenId.toString());
-  token.auctionID = "0";
-  auction.action = "Auction Cancelled";
-  auction.auctionID = "0";
-  auction.save();
-}
-
-export function handleReserveAuctionFinalized(
-  event: ReserveAuctionFinalizedEvent
-): void {
-  let auction = Auction.load(event.params.auctionId.toString());
-  if (auction) {
-    auction.action = "Auction ended";
-    auction.bidder = event.params.bidder;
-
-    let token = Minted.load(auction.tokenId.toString());
-    token.owner = event.params.bidder;
-    token.save();
-    auction.save();
-  }
->>>>>>> 24efe02af8ac40ae70aa04bf733d35e6567ee47e
 }
